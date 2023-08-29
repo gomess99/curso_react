8480a8e809e9bfb88e24ecfab6e8ce73cd44f027
Gabriel Gomes <gomess99@outlook.com>
2023-08-29 15:43:08 -0300

    7. Conhecendo useStates

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
-import { Header } from "./components/header";
-import { Aluno } from "./components/aluno";
-import { Footer } from "./components/footer";
+
+import { useState } from "react"
 
 export default function App(){
+  const [input, setInput] = useState("")
+  const [idade, setIdade] = useState("")
+  const [aluno, setAluno] = useState("Digite no campo a cima")
   //aqui está send exportando por padrão (usando export default) uma função chamada App. Essa função é o componente principal do aplicativo
+
+  function mostrarAluno(){
+    
+    setAluno(input);
+  }
 
   return(
     //O componente 'App' retorna um JSX (JavaScript XML), que é uma sintaxe semelhante ao HTML usado pelo React para descrever a estrutura da interface do usuário. 
     <div>
-      <Header title="Curso React + TypeScript"/>
+      <h1>
+        Conhecendo useStates
+      </h1>
 
-      <Aluno nome="Carlos Gomes" idade={52}/>
+      <input
+        placeholder="Digite o nome" 
+        value={input}
+        onChange={ (e) => setInput(e.target.value)}
+      />
 
-      <Aluno nome="Gabriel Gomes" idade={20}/>
+      <br />
+      <br />
 
-      <Aluno nome="Cleusa Gomes" idade={51}/>
+      <input
+        placeholder="Digite a idade" 
+        value={idade}
+        onChange={ (e) => setIdade(e.target.value)}
+      />
 
-      <Footer/>
+      <br />
+      <br />
+
+      <button onClick={mostrarAluno}>Mostrar Aluno</button>
+
+      <hr />
+
+      <h3>Bem vindo: {aluno}</h3>
     </div>
   )
 }
