8dc556e3de360149e7496297aeccb8937414d2bf
Gabriel Gomes <gomess99@outlook.com>
2023-08-29 20:26:25 -0300

    9. Fixando useStates

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
 
 import { useState } from "react"
 
-interface InfoAlunoProps{
+interface UserProps{
   nome: string;
-  idade: string;
+  cargo: string;
 }
 
 export default function App(){
-  const [input, setInput] = useState("")
-  const [idade, setIdade] = useState("");
-  
-  const [infoAluno, setInfoAluno] = useState<InfoAlunoProps>();
 
-  const [contador, setContador] = useState(0);
+  const [user, setUser] = useState <UserProps>({
+    nome: "Visitante",
+    cargo: ""
+  })
 
-  function mostrarAluno(){
-    setInfoAluno({
-      nome: input,
-      idade: idade,
-    });
+  function handleLogin(){
+    setUser({
+      nome: "Sujeito",
+      cargo: "Programador"
+    })
   }
 
-  function adicionar(){
-    setContador(valorAtual => valorAtual +1);
-  }
-
-  function diminuir(){
-    if(contador === 0){
-      return;
-    }
-    setContador(valorAtual => valorAtual -1);
+  function handleLogout(){
+    setUser({
+      nome: "Visitante",
+      cargo: ""
+    })
   }
 
   return(
@@ -38,37 +33,14 @@ export default function App(){
       <h1>
         Conhecendo useStates
       </h1>
-
-      <input
-        placeholder="Digite o nome" 
-        value={input}
-        onChange={ (e) => setInput(e.target.value)}
-      />
-
-      <br />
-      <br />
-
-      <input
-        placeholder="Digite a idade" 
-        value={idade}
-        onChange={ (e) => setIdade(e.target.value)}
-      />
-
-      <br />
-      <br />
-
-      <button onClick={mostrarAluno}>Mostrar Aluno</button>
-
-      <hr />
-
-      <h3>Bem vindo: {infoAluno?.nome}</h3>
-      <h3>Sua idade: {infoAluno?.idade}</h3>
-
-      <hr />
-      <br />
-      <h1>Contador com useState</h1>
-
-      <button onClick={adicionar}> + </button> {contador}  <button onClick={diminuir}> - </button>
+      <button onClick={handleLogin}>
+        Entrar
+      </button>
+      <button onClick={handleLogout}>
+        Sair
+      </button>
+      <h4>{user.nome}</h4>
+      <strong>{user.cargo}</strong>
     </div>
   );
 }
